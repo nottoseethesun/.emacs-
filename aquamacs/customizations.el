3340cb27f1e19ffbd9d788d2b01557e9838e1185
Christopher M Balz <christopherbalz@yahoo.com>
2013-06-14 22:47:37 -0600

    Update customizations.el
    Removes the unnecessary firefox bindings.

--- aquamacs/customizations.el
+++ aquamacs/customizations.el
@@ -2,17 +2,15 @@
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.  
+  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["black" "red" "green" "yellow" "pink" "magenta" "cyan" "white"])
  '(appt-message-warning-time 60 t)
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-customization-version-id 0 t)
  '(aquamacs-tool-bar-user-customization nil t)
- '(browse-url-browser-function (quote browse-url-firefox-new-tab))
- '(browse-url-generic-program "~/utils/firefox/firefox")
  '(calculator-number-digits 10)
  '(case-fold-search t)
- '(current-language-environment "Latin-1")            ;; The 'Green' part below fixes an issue where the Help Window and other child windows dobn't take the values set in '.emacs':      
+ '(current-language-environment "Latin-1")            ;; The 'Green' part below fixes an issue where the Help Window and other child windows dobn't take the values set in '.emacs':
  '(default-frame-alist (quote ((tool-bar-lines . 0) (menu-bar-lines . 1) (background-color . "Gray10") (foreground-color . "Green") (left . 0) (width . 141) (height . 44))))
  '(default-input-method "latin-1-prefix")
  '(dired-recursive-deletes (quote top))
@@ -27,8 +25,6 @@
  '(midnight-mode t nil (midnight))
  '(ns-tool-bar-display-mode nil t)
  '(ns-tool-bar-size-mode nil t)
- '(printer-name "USB001")
- '(smtpmail-default-smtp-server "smtp.earthlink.net")
  '(speedbar-show-unknown-files t)
  '(speedbar-use-images t)
  '(standard-indent 4)
