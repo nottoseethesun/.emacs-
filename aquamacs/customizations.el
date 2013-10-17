03de8ff553f5998deab0a843fcde7a4f6da2e763
Christopher M Balz <christopherbalz@yahoo.com>
2013-10-17 11:14:39 -0700

    Update customizations.el
    The latest; has some speedbar options.

--- aquamacs/customizations.el
+++ aquamacs/customizations.el
@@ -1,33 +1,47 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Only Aquamacs needs this.
   ;; This file is found at: ~/Library/Preferences/Aquamacs Emacs/customizations.el
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(adaptive-fill-mode t)
  '(ansi-color-names-vector ["black" "red" "green" "yellow" "pink" "magenta" "cyan" "white"])
- '(appt-message-warning-time 60)
+ '(appt-message-warning-time 60 t)
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-customization-version-id 215 t)
  '(aquamacs-tool-bar-user-customization (quote ((16777249 new-file open-file recent-files save-buffer aquamacs-print nil undo redo cut copy paste isearch-forward nil customize help))) t)
  '(calculator-number-digits 10)
  '(case-fold-search t)
+ '(column-number-mode t)
  '(current-language-environment "Latin-1")
  '(default-frame-alist (quote ((left . 0) (width . 141) (height . 44) (modeline . t) (mouse-color . "yellow") (background-mode . dark) (tool-bar-lines . 0) (menu-bar-lines . 1) (right-fringe . 11) (left-fringe . 10) (cursor-color . "Deep Pink") (background-color . "Gray10") (foreground-color . "Green") (font . "-apple-Courier-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1") (fontsize . 0) (font-backend ns))))
  '(default-input-method "latin-1-prefix")
  '(dired-recursive-deletes (quote top))
  '(display-battery-mode t)
  '(display-time-mode t)
+ '(fill-column 90)
  '(font-use-system-font t)
  '(frame-background-mode nil)
+ '(global-font-lock-mode t nil (font-lock))
  '(indent-tabs-mode nil)
  '(js2-mode-escape-quotes nil)
+ '(less-css-lessc-command "lessc")
+ '(less-css-lessc-options (quote ("--no-color" "--strict-math=on" "--strict-units=on")))
+ '(mail-host-address "")
+ '(markdown-command "open -a /Applications/Marked.app")
+ '(markdown-command-needs-filename t)
  '(midnight-delay 34200)
  '(midnight-hook (quote (update-my-calendar)))
  '(midnight-mode t nil (midnight))
  '(ns-tool-bar-display-mode (quote both) t)
  '(ns-tool-bar-size-mode (quote regular) t)
+ '(printer-name "USB001")
+ '(rng-nxml-auto-validate-flag nil)
+ '(safe-local-variable-values (quote ((eval checkdoc-minor-mode 1))))
  '(scss-compile-at-save nil)
+ '(show-paren-mode t nil (paren))
+ '(smtpmail-default-smtp-server "smtp.earthlink.net")
  '(speedbar-show-unknown-files t)
  '(speedbar-use-images t)
  '(standard-indent 4)
